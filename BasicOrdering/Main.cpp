3f2fe13316730d9079c0cc71915b9d8a9ec9d6e6
LouisStefanski-dev <78240770+LouisStefanski-dev@users.noreply.github.com>
2021-02-26 18:31:11 -0500

    Added a comment for TODO

--- BasicOrdering/Main.cpp
+++ BasicOrdering/Main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "Inventory.h"
 
+//TODO:
+//		Add MySql server functions
+
 int main() {
 	Inventory myInventory;
 	myInventory.createNewInventoryItem("egg", 5, 2, 2.1, 0);
